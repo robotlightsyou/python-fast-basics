fd45a05b3f9cd2891a33b9db168c42ec176264d9
robotlightsyou <robotlightsyou@gmail.com>
2021-03-06 14:03:14 -0800

    lessons displaying correctly

--- pysite/lessons/views.py
+++ pysite/lessons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from . import models
 
@@ -16,5 +16,6 @@ class LessonDetailView(DetailView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["elements"] = models.Lesson_Content.objects.filter(lesson__name="Integers").order_by("item_num")
+        name = get_object_or_404(models.Lesson, slug=self.kwargs.get('slug'))
+        context["elements"] = models.Lesson_Content.objects.filter(lesson=name).order_by("item_num")
         return context
