228a59af26b8b0de51490fac4a8d5edc58c5c53f
robotlightsyou <robotlightsyou@gmail.com>
2021-04-05 17:13:41 -0700

    cash register

--- pt/sessions/003 session-numbers/exercises/cash_register_video.py
+++ pt/sessions/003 session-numbers/exercises/cash_register_video.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
 '''
-write a function that will ask for the user for input which will be an amount of money,
-then return the minimum number of coins
+write a function that will ask for the
+user for input which will be an amount
+of money, then return the minimum 
+number of coins.
 '''
 
 user_coins = [25, 10, 5, 1]
@@ -11,16 +13,12 @@ def main():
     print(get_change())
 
 def get_change():
+
     # take input
     print("How much change is owed?")
     amount = input("> ")
     # modify input from integer
     amount = int(float(amount) * 100)
-    total_coins = 0 
-    for coin in user_coins:
-        total_coins += amount // coin
-        amount %= coin
-    return total_coins
 
     # find least number of coins
     # coins = 0 
@@ -31,8 +29,15 @@ def get_change():
     # coins += amount // 5 # -> 1
     # amount %= 5 # 1
     # coins += amount
-
     # return coins
+
+    # find least number of coins
+    total_coins = 0 
+    for coin in user_coins:
+        total_coins += amount // coin
+        amount %= coin
+    return total_coins
+
 
 
 if __name__ == '__main__':
